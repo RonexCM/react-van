a5bc2f1fe399b8288c4d9e7057a1f89d9544971a
RonexCM <98376365+RonexCM@users.noreply.github.com>
2024-01-14 22:54:06 +0545

    upto video 15

--- src/pages/Host/HostVan.jsx
+++ src/pages/Host/HostVan.jsx
@@ -1,46 +1,33 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const HostVan = () => {
-
-  const[vans,setVans] = useState([])
+  const [vans, setVans] = useState([]);
   useEffect(() => {
     fetch("/api/host/vans")
-    .then((res) => res.json())
-    .then((data) => setVans(data.vans))
-  })
+      .then((res) => res.json())
+      .then((data) => setVans(data.vans));
+  });
 
-  const hostEls = vans.map(van => (
-    <Link 
-    to={`/host/vans/${van.id}`}
-    key={van.id}
-    className="host-van-link-wrapper"
-    >
+  const hostEls = vans.map((van) => (
+    <Link to={van.id} key={van.id} className="host-van-link-wrapper">
       <div className="host-van-single" key={van.id}>
-        <img src={van.imageUrl} alt={`Photo of ${van.name}`}/>
+        <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
         <div className="host-van-info">
           <h3>{van.name}</h3>
           <p>${van.price}/day</p>
         </div>
       </div>
     </Link>
-  ))
+  ));
   return (
     <div className="listedVans">
       <h1 className="host-van-title">Your Listed Vans</h1>
       <div className="host-vans-list">
-        {
-          vans.length > 0 ? (
-            <section>
-              {hostEls}
-            </section>
-          ) : (
-          <h2>Loading...</h2>
-        )
-        }
+        {vans.length > 0 ? <section>{hostEls}</section> : <h2>Loading...</h2>}
       </div>
-      </div>
-  )
-}
+    </div>
+  );
+};
 
-export default HostVan
+export default HostVan;
--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,37 +1,92 @@
-import React, { useEffect, useState } from 'react';
-import "../../server.js"
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import "../../server.js";
+import { Link, useSearchParams } from "react-router-dom";
 
 const Vans = () => {
-  const [vans,setVans] = useState([]);
+  const [search, setSearch] = useSearchParams();
+  const [vans, setVans] = useState([]);
+  const typeFilter = search.get("type");
 
   useEffect(() => {
     fetch("/api/vans")
       .then((res) => res.json())
-      .then((data) => setVans(data.vans))
-      .catch(error => console.error('Error fetching data:', error));
+      .then((data) => {
+        console.log("🚀 ~ useEffect ~ data:", data);
+        return setVans(data.vans);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
   }, []); // Removed 'data' from the dependency array
 
+  const filteredData = typeFilter
+    ? vans.filter((van) => van.type.toLowerCase() === typeFilter)
+    : vans;
 
-  const vanElements = vans.map(van => (
+  const vanElements = filteredData.map((van) => (
     <div key={van.id} className="van-title">
-    <Link to={`/vans/${van.id}`}>
-        <img src={van.imageUrl} alt='vans-picture'/>
+      <Link to={van.id}>
+        <img src={van.imageUrl} alt="vans-picture" />
         <div className="van-info">
-            <h3>{van.name}</h3>
-            <p>${van.price}<span>/day</span></p>
-            <i className={`van-type ${van.type} selected`}>{van.type}</i>
+          <h3>{van.name}</h3>
+          <p>
+            ${van.price}
+            <span>/day</span>
+          </p>
+          <i className={`van-type ${van.type} selected`}>{van.type}</i>
         </div>
-        </Link>
+      </Link>
     </div>
-  ))
+  ));
+
+  function handleFilterChange(key, value) {
+    setSearch((prevParams) => {
+      if (value === null) {
+        prevParams.delete(key);
+      } else {
+        prevParams.set(key, value);
+      }
+      return prevParams;
+    });
+  }
 
   return (
     <div className="van-list-container">
-    <h1>Explore our van options</h1>
-    <div className="van-list">
-      {vanElements}
-    </div>
+      <h1>Explore our van options</h1>
+      <div className="van-list-filter-buttons">
+        <button
+          onClick={() => handleFilterChange("type", "simple")}
+          className={`van-type simple ${
+            typeFilter === "simple" ? "selected" : ""
+          }`}
+        >
+          Simple
+        </button>
+        <button
+          className={`van-type simple ${
+            typeFilter === "luxury" ? "selected" : ""
+          }`}
+          onClick={() => handleFilterChange("type", "luxury")}
+        >
+          Luxury
+        </button>
+        <button
+          className={`van-type simple ${
+            typeFilter === "rugged" ? "selected" : ""
+          }`}
+          onClick={() => handleFilterChange("type", "rugged")}
+        >
+          Rugged
+        </button>
+
+        {typeFilter ? (
+          <button
+            className="van-type clear-filters"
+            onClick={() => setSearch({})}
+          >
+            Clear filters
+          </button>
+        ) : null}
+      </div>
+      <div className="van-list">{vanElements}</div>
     </div>
   );
 };
