62aaec7f6982074f146a349916bb45698cd01c44
RonexCM <98376365+RonexCM@users.noreply.github.com>
2024-02-29 22:32:44 +0545

    added login

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,40 @@
-import React from 'react'
-import { Link,NavLink } from 'react-router-dom'
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import { CgProfile } from "react-icons/cg";
 
 const Header = () => {
   return (
     <div>
-       <header>
-      <Link className="logo" to='/'>#VANLIFE</Link>
-    <nav>
-      <NavLink 
-      className={({isActive}) => isActive ? "active-link" : null }
-      to='/host'>Host </NavLink>
-      <NavLink 
-      className={({isActive}) => isActive ? "active-link" : null }
-      to='/about'>About</NavLink>
-      <NavLink 
-      className={({isActive}) => isActive ? "active-link" : null }
-      to='/vans'>Vans</NavLink>
-    </nav>
-    </header>
+      <header>
+        <Link className="logo" to="/">
+          #VANLIFE
+        </Link>
+        <nav>
+          <NavLink
+            className={({ isActive }) => (isActive ? "active-link" : null)}
+            to="/host"
+          >
+            Host{" "}
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? "active-link" : null)}
+            to="/about"
+          >
+            About
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? "active-link" : null)}
+            to="/vans"
+          >
+            Vans
+          </NavLink>
+          <Link to="login" className="login-link">
+            <CgProfile className="login-icon"/>
+          </Link>
+        </nav>
+      </header>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
